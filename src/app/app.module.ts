65bd05416cffc02a851b547286c675301b048880
Mateusz Makowski <matmakos@gmail.com>
2020-02-22 00:18:02 +0100

    Update root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecordsComponent } from './records/records.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecordsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
