73763fbb14eb8ac6d34a00051c3f901c918564c5
Mateusz Makowski <matmakos@gmail.com>
2020-03-12 21:00:11 +0100

    Move login logic to login component

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,21 +6,9 @@ import { UserService } from '../user.service';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-
   constructor(
-    private userService: UserService
-  ) { }
-
-  login(username: string, password: string) {
-    this.userService.login(username, password);
-  }
-
+  ) {}
 
   ngOnInit(): void {
-    this.userService.loginEvent.subscribe((event) => {
-      if (event) {
-        location.reload();
-      }
-    })
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  login(username: string, password: string) {
+    this.userService.login(username, password);
   }
 
+  constructor(
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.userService.loginEvent.subscribe((event) => {
+      if (event) {
+        location.reload();
+      }
+    });
+  }
 }
