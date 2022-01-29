c3b8c78ff8b9fda9f2ab7ef89e4c50a96b716b07
Mateusz Makowski <matmakos@gmail.com>
2022-01-29 13:16:19 +0100

    Change to localhost

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 export class Config {
-  //private static apiBaseUrl: string = 'http://localhost:3000';
-  private static apiBaseUrl: string = 'http://192.168.1.200:3000';
+  private static apiBaseUrl: string = 'http://localhost:3000';
+  //private static apiBaseUrl: string = 'http://192.168.1.200:3000';
   private static apiIncomeUrl: string = '/api/v1/income';
   private static apiExpensesUrl: string = '/api/v1/expenses';
   private static apiLoginUrl: string = '/api/v1/login';
