22cffcdf2b373fe9b7466db40937e2a242eebe31
Mateusz Makowski <matmakos@gmail.com>
2022-02-05 10:14:54 +0100

    Remove unused code

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -54,7 +54,3 @@ import 'zone.js';  // Included with Angular CLI.
 (window as any).process = { env: { DEBUG: undefined }, };
 (window as any).global = window;
 global.Buffer = global.Buffer || require('buffer').Buffer;
-// (window as any).process = {
-//     version: ''
-    
-// };
